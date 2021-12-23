07113758bbd58d6a1e1d15678776d47d6b14a7a5
zcliu <zhicheng.liu@gmail.com>
2021-12-23 15:12:43 -0500

    v1.5.3

--- static/demo/groupedBarChart.js
+++ static/demo/groupedBarChart.js
@@ -3,19 +3,16 @@ let rect = scn.mark("rect", {top:100, left: 100, width: 16, height: 200, fillCol
 let dt = await atlas.csv("csv/olympic-medals.csv");
 
 let medals = scn.repeat(rect, dt, {field: "Medal_Type"});
-medals.layout = atlas.layout("grid", {numCols: 3, colGap: 1});
+medals.layout = atlas.layout("grid", {numRows: 1, colGap: 1});
 
 let bars = scn.repeat(medals, dt, {field: "Country_Code"});
 //todo: handle encoding before 2nd repeat
 let htEncoding = scn.encode(rect, {field: "Count", channel: "height"});
 scn.encode(rect, {field: "Medal_Type", channel: "fillColor", mapping: {"Gold": "#c9b037", "Silver": "#d7d7d7", "Bronze": "#ad8a56"}});
 
-bars.layout = atlas.layout("grid", {numRows: 5, colGap: 20, rowGap: 15});
-bars.layout.colGap = 15;
-bars.layout.numCols = 10;
+bars.layout = atlas.layout("grid", {numRows: 2, colGap: 20, rowGap: 15});
 
-scn.axis("x", "Country_Code", {orientation: "bottom"});
-scn.legend("fillColor", "Medal_Type", {x: 800, y: 100});
-
-//TODO: fix bug in following line
-//scn.axis("height", "Count", {"orientation": "left", "x-coordinate": 130});
+let axis = scn.axis("x", "Country_Code", {orientation: "bottom"});
+scn.axis("height", "Count", {"orientation": "left", item: bars.firstChild.firstChild});
+scn.axis("height", "Count", {"orientation": "left", item: bars.lastChild.firstChild});
+let legend = scn.legend("fillColor", "Medal_Type", {x: 800, y: 100});
--- static/demo/isotype.js
+++ static/demo/isotype.js
@@ -6,4 +6,4 @@ let c = scene.repeat(rect, dt);
 let bins = scene.classify(c.children, "Age Bin", c);
 bins.forEach(d => d.layout = atlas.layout("grid", {numCols: 15, rowGap: 5, hgap: 10}));
 c.layout = atlas.layout("stack", {orientation: "vertical", gap: 30});
-scene.axis("y", "Age Bin", {orientation: "left", tickVisible: false});
+scene.axis("y", "Age Bin", {orientation: "left", tickVisible: false, pathVisible: false});
