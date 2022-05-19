3f1a4f7d9b466f7193eb99d30436a075f6ec9178
zcliu <zhicheng.liu@gmail.com>
2022-05-19 13:20:34 -0400

    v1.6.5

--- static/demo/roseChart.js
+++ static/demo/roseChart.js
@@ -1,11 +1,13 @@
 let scn = atlas.scene({fillColor: "#FFFAFC"});
-let circ = scn.mark("circle", {radius: 120, x: 300, y: 300, fillColor: "orange", strokeColor: "white"});
+let circ = scn.mark("circle", {radius: 120, x: 400, y: 400, fillColor: "orange", strokeColor: "white"});
 let data = await atlas.csv("csv/nightingale.csv");
 
-//need to handle divide angular then radial
-let rings = scn.divide(circ, data, {field: "Type", orientation: "radial"});
-let arcs = scn.divide(rings.firstChild, data, {field: "Month", orientation: "angular"});
+// let rings = scn.divide(circ, data, {field: "Type", orientation: "radial"});
+// let arcs = scn.divide(rings.firstChild, data, {field: "Month", orientation: "angular"});
+
+let pies = scn.divide(circ, data, {field: "Month", orientation: "angular"});
+let arcs = scn.divide(pies.firstChild, data, {field: "Type", orientation: "radial"});
 
 scn.encode(arcs.firstChild, {field: "Type", channel: "fillColor", mapping: {"Disease": "#CFDFE3", "Wounds": "#EBC3BE", "Other": "#77746F"}});
-scn.encode(arcs.firstChild, {field: "Death", channel: "thickness", rangeExtent: 200});
+scn.encode(arcs.firstChild, {field: "Death", channel: "thickness", scaleType: "sqrt", rangeExtent: 200});
 scn.legend("fillColor", "Type", {x: 150, y: 100});
